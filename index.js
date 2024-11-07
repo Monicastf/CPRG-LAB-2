c7ec5097b7822bf030aa00d679fd43f3e2f03495
Monicastf <monicas.f555@gmail.com>
2024-11-06 17:01:11 -0700

    lalala

---
+++ index.js
@@ -0,0 +1,20 @@
+bear.addEventListener("click", function(){
+    console.log("Picture of bear");
+    document.getElementById("animal").src = "photos/bear.webp";
+})
+elk.addEventListener("click", function(){
+    console.log("Picture of elk");
+    document.getElementById("animal").src = "photos/elk.webp";
+})
+moose.addEventListener("click", function(){
+    console.log("Picture of moose");
+    document.getElementById("animal").src = "photos/moose.webp";
+})
+sheep.addEventListener("click", function(){
+    console.log("Picture of sheep");
+    document.getElementById("animal").src = "photos/sheep.webp";
+})
+wolf.addEventListener("click", function(){
+    console.log("Picture of wolf");
+    document.getElementById("animal").src = "photos/wolf.webp";
+})
